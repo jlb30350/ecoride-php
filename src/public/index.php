09739ba37a21b332e0f981c553b0999563a513a3
Jean-Luc Bonneville <jeanlucbonneville@gmail.com>
2025-08-30 23:50:22 +0200

    ajout tiret du six

--- src/public/index.php
+++ src/public/index.php
@@ -1,24 +1,26 @@
-<?php require_once __DIR__."/../config/db.php"; ?>
-<?php require_once __DIR__."/../lib/auth.php"; ?>
+<?php require_once __DIR__ . "/../config/db.php"; ?>
+<?php require_once __DIR__ . "/../lib/auth.php"; ?>
 <!doctype html>
 <html>
+
 <head>
   <meta charset="utf-8">
-  <title>EcoRide</title>
+  <title>EcoRide-php</title>
   <link rel="stylesheet" href="/styles.css">
 </head>
+
 <body>
-<?php require_once __DIR__."/partials/nav.php"; ?>
-<h1>EcoRide</h1>
-<p>Chercher un trajet</p>
-<form method="get" action="/rides_search.php">
-  <input name="origin" placeholder="Départ">
-  <input name="destination" placeholder="Arrivée">
-  <input type="week" name="ride_week" placeholder="Semaine (YYYY-Www)">
-<!-- tu peux garder l'ancien champ jour si tu veux, mais un seul suffit -->
+  <?php require_once __DIR__ . "/partials/nav.php"; ?>
+  <h1>EcoRide</h1>
+  <p>Chercher un trajet</p>
+  <form method="get" action="/rides_search.php">
+    <input name="origin" placeholder="Départ">
+    <input name="destination" placeholder="Arrivée">
+    <input type="week" name="ride_week" placeholder="Semaine (YYYY-Www)">
+    <!-- tu peux garder l'ancien champ jour si tu veux, mais un seul suffit -->
 
-  <button>Rechercher</button>
-</form>
+    <button>Rechercher</button>
+  </form>
 </body>
-</html>
 
+</html>
